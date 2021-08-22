747d6029a2f95fecff6d82a5b50eb435ef966daf
leonardo.martins <leonardo.martins@squadra.com.br>
2021-08-22 17:36:07 -0300

    O básico sobre testes de unidade; e Asserções (Assert)

---
+++ 01 - Testes de Unidade/Demo.Tests/07 - AssertingCollectionsTests.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class AssertingCollectionsTests
+    {
+        [Fact]
+        public void FuncionarioFactory_Habilidades_NaoDevePossuirHabilidadesVazias()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Eduardo", 10000);
+
+            // Assert
+            Assert.All(funcionario.Habilidades, habilidade => Assert.False(string.IsNullOrWhiteSpace(habilidade)));
+        }
+
+        [Fact]
+        public void FuncionarioFactory_Habilidades_JuniorDevePossuirHabilidadeBasicas()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Eduardo", 1000);
+
+            // Assert
+            Assert.Contains("OOP", funcionario.Habilidades);
+        }
+
+        [Fact]
+        public void FuncionarioFactory_Habilidades_JuniorNaoDevePossuirHabilidadeAvancada()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Eduardo", 1000);
+
+            // Assert
+            Assert.DoesNotContain("Microservices", funcionario.Habilidades);
+        }
+
+        [Fact]
+        public void FuncionarioFactory_Habilidades_SeniorDevePossuirTodasHabilidades()
+        {
+            // Arrange & Act
+            var funcionario = FuncionarioFactory.Criar("Eduardo", 15000);
+
+            var habilidades = new[]
+            {
+                "Lógica de Programação",
+                "OOP",
+                "Testes",
+                "Microservices"
+            };
+
+            // Assert
+            Assert.Equal(habilidades, funcionario.Habilidades);
+        }
+    }
+}
---
+++ 01 - Testes de Unidade/Demo/Funcionario.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo
+{
+    public class Pessoa
+    {
+        public string Nome { get; protected set; }
+        public string Apelido { get; set; }
+    }
+
+    public class Funcionario : Pessoa
+    {
+        public double Salario { get; private set; }
+        public NivelProfissional NivelProfissional { get; private set; }
+        public IList<string> Habilidades { get; set; }
+
+        public Funcionario(string nome, double salario)
+        {
+            Nome = string.IsNullOrEmpty(nome) ? "Fulano" : nome;
+
+            DefinirSalario(salario);
+            DefinirHabilidades();
+        }
+
+        public void DefinirSalario(double salario)
+        {
+            if (salario < 500)
+            {
+                throw new Exception("Salario inferior ao permitido");
+            }
+
+            Salario = salario;
+
+            if (salario < 2000)
+            {
+                NivelProfissional = NivelProfissional.Junior;
+            }
+            else if (salario >= 2000 && salario < 8000)
+            {
+                NivelProfissional = NivelProfissional.Pleno;
+            }
+            else if (salario >= 8000)
+            {
+                NivelProfissional = NivelProfissional.Senior;
+            }
+        }
+
+        public void DefinirHabilidades()
+        {
+            var habilidadesBasicas = new List<string>
+            {
+                "Lógica de Programação",
+                "OOP"
+            };
+
+            Habilidades = habilidadesBasicas;
+
+            switch (NivelProfissional)
+            {
+                case NivelProfissional.Pleno:
+                    habilidadesBasicas.Add("Testes");
+                    break;
+                case NivelProfissional.Senior:
+                    habilidadesBasicas.Add("Testes");
+                    habilidadesBasicas.Add("Microservices");
+                    break;
+            }
+        }
+    }
+
+    public enum NivelProfissional
+    {
+        Junior,
+        Pleno,
+        Senior
+    }
+
+    public class FuncionarioFactory
+    {
+        public static Funcionario Criar(string nome, double salario)
+        {
+            return new Funcionario(nome, salario);
+        }
+    }
+}
