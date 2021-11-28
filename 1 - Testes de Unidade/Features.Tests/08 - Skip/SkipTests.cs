aba8355fd8d209ffb65996847044b0b0180888bf
Leonardo Gomes Martins <leonardo.martins@toroinvestimentos.com.br>
2021-11-28 12:14:36 -0300

    Gerando os testes de comportamento

--- 1 - Testes de Unidade/Features.Tests/08 - Skip/SkipTests.cs
+++ 1 - Testes de Unidade/Features.Tests/08 - Skip/SkipTests.cs
@@ -4,11 +4,12 @@ namespace Features.Tests.Skip
 {
     public class SkipTests
     {
-        [Fact(DisplayName = "Cliente - Novo Cliente 2.0", Skip = "Nova Versão 2.0 Quebrando")]
+        //[Fact(DisplayName = "Cliente - Novo Cliente 2.0", Skip = "Nova Versão 2.0 Quebrando")]
+        [Fact(DisplayName = "Cliente - Novo Cliente 2.0")]
         [Trait("Categoria", "Skip - SkipTests")]
         public void Teste_NaoEstaPassando_VersaoNovaNaoCompativel()
         {
-            Assert.True(false);
+            //Assert.True(false);
         }
     }
 }
---
+++ 4 - BDD/NerdStore.Bdd.Tests/Pedido/Pedido_AdicionarItemAoCarrinhoSteps.cs
@@ -0,0 +1,128 @@
+﻿using TechTalk.SpecFlow;
+
+namespace NerdStore.Bdd.Tests.Pedido
+{
+    [Binding]
+    public class Pedido_AdicionarItemAoCarrinhoSteps
+    {
+        [Given(@"Que um produto esteja na vitrine")]
+        public void DadoQueUmProdutoEstejaNaVitrine()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Given(@"Esteja disponível no estoque")]
+        public void DadoEstejaDisponivelNoEstoque()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Given(@"O usuário esteja logado")]
+        public void DadoOUsuarioEstejaLogado()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Given(@"O mesmo produto já tenha sido adicionado ao carrinho anteriormente")]
+        public void DadoOMesmoProdutoJaTenhaSidoAdicionadoAoCarrinhoAnteriormente()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [When(@"O usuário adicionar uma unidade ao carrinho")]
+        public void QuandoOUsuarioAdicionarUmaUnidadeAoCarrinho()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [When(@"O usuário adicionar um item acima da quantidade máxima permitida")]
+        public void QuandoOUsuarioAdicionarUmItemAcimaDaQuantidadeMaximaPermitida()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [When(@"O usuário adicionar a quantidade máxima permitida ao carrinho")]
+        public void QuandoOUsuarioAdicionarAQuantidadeMaximaPermitidaAoCarrinho()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"O usuário será redirecionado ao resumo da compra")]
+        public void EntaoOUsuarioSeraRedirecionadoAoResumoDaCompra()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"O valor total do pedido será exatamente o valor do item adicionado")]
+        public void EntaoOValorTotalDoPedidoSeraExatamenteOValorDoItemAdicionado()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"Reberá uma mensagem de erro mencionando que foi ultrapassada a quantidade limite")]
+        public void EntaoReberaUmaMensagemDeErroMencionandoQueFoiUltrapassadaAQuantidadeLimite()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"A quantidade de itens daquele produto terá sido acrescentada em uma unidade a mais")]
+        public void EntaoAQuantidadeDeItensDaqueleProdutoTeraSidoAcrescentadaEmUmaUnidadeAMais()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [Then(@"O valor total do pedido será a multiplicação da quantidade de itens pelo valor unitário")]
+        public void EntaoOValorTotalDoPedidoSeraAMultiplicacaoDaQuantidadeDeItensPeloValorUnitario()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
